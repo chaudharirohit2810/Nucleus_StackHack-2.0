4751faf82d0bdf85a9e2d17829254d11a74ef16f
VirajJadhav <virajssjadhav@gmail.com>
2021-01-20 21:13:47 +0530

    feat: bonus tab view HR

--- client/src/pages/HR/bonus/index.jsx
+++ client/src/pages/HR/bonus/index.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
-import { Typography } from "antd";
+import { Typography, Tabs } from "antd";
 import BonusTable from "./bonusTable";
 
 const { Title } = Typography;
+const { TabPane } = Tabs;
 
 class Bonus extends Component {
     render() {
         return (
             <div>
                 <Title level={2}>Bonus</Title>
-                <BonusTable />
+                <Tabs
+                    defaultActiveKey="1"
+                    size="large"
+                    style={{ marginBottom: 32 }}
+                >
+                    <TabPane tab="Lend" key="1">
+                        Lend Bonus
+                    </TabPane>
+                    <TabPane tab="Requested" key="2">
+                        <BonusTable />
+                    </TabPane>
+                </Tabs>
             </div>
         );
     }
